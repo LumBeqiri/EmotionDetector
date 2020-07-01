a6659833f8a6fcb088e036a152ef3f10321806fe
Betim Sherifi <betimsh99@gmail.com>
2020-07-01 17:58:06 +0200

    created a header & fixed the relation between the canvas and the video panel

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ function startVideo() {
 
 video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video);
-    document.body.append(canvas);
+    const mydiv = document.querySelector(".video-panel")
+    mydiv.appendChild(canvas)
     const displaySize = { width: video.width, height: video.height }
     faceapi.matchDimensions(canvas, displaySize)
     setInterval(async () => {
